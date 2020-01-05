9c3e89ef037372ddde60fbf7f425f77fb34e897d
Renat <58852727+awuce@users.noreply.github.com>
2020-01-06 05:13:11 +0700

    Localization has risen

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Blog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    #config.time_zone = 'Novosibirsk'
-    #config.i18n.default_locale = :ru
+    config.time_zone = 'Novosibirsk'
+    config.i18n.default_locale = :ru
   end
 end
